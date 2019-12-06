087b35ec9d7a86aa08c46b62460498ffae3d708b
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-06 00:12:52 -0800

    Will create html for each team member

--- app.js
+++ app.js
@@ -33,40 +33,9 @@ const engineerQ = [
 ];
 
 
-const EmployeeQuestions = [ 
-    {
-        type: "input",
-        message: "What is the employee's name?",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "What is the employee's id?",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "What is the employee's Email?",
-        name: "email"
-    },
-    {
-        type: "list",
-        message: "what the employee's title?",
-        name: "title",
-        choices: ["Engineer", "Intern", "Manager"]
-    }
-];
-
-
-// inquirer.prompt(managerQ).then(consoleLog)
-
-// function consoleLog(response){
-//     console.log(response);
-// }
-
-
 async function start(){
     console.log("Let's make your Dream Team!");
+    let teamHTML = "";
 
     await inquirer.prompt(
         {
@@ -127,28 +96,117 @@ async function start(){
                 await inquirer.prompt(managerQ)
                 .then((data) => {
                     const manager = new Manager(name, id, email, data.officeNo);
-                    team.push(manager);
+
+                    teamMember = fs.readFileSync("templates/manager.html");
+            
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+
+                    // team.push(manager);
                 });
                 break;
             case "Intern":
                 await inquirer.prompt(internQ)
                 .then((data) => {
                     const intern = new Intern(name, id, email, data.school);
-                    team.push(intern);
+
+                    teamMember = fs.readFileSync("templates/intern.html");
+            
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
                 });
                 break;
             case "Engineer":
                 await inquirer.prompt(engineerQ)
                 .then((data) => {
                     const engineer = new Engineer(name, id, email, data.github);
-                    team.push(engineer);
+
+                    teamMember = fs.readFileSync("templates/engineer.html");
+            
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+
                 });
                 break;
         }
     }
 
-    console.log(team[0].name);
-    console.log(team[0].getRole());
+    //console.log(teamMember);
+
+    
+
+
+    // for (i=0; i < team.length; i++){
+    //     let teamMember;
+
+    //     if (team[i].getRole() === "Manager"){
+
+    //         // console.log(team[i].name);
+    //         // console.log(team[i].getRole());
+    //         // console.log(team[i].id);
+    //         // console.log(team[i].email);
+    //         // console.log(team[i].officeNumber);
+
+    //         teamMember = fs.readFileSync("templates/manager.html");
+            
+    //         teamHTML = teamHTML + eval('`'+ teamMember +'`');
+
+    //         // fs.readFileSync("templates/engineer.html", "utf8", (err, htmlFile) => {
+    //         //     if (err) { return console.log(err); }
+            
+    //         //     teamMember = eval('`'+ htmlFile +'`');
+    //         // });
+    //     }
+    //     else if (team[i].getRole() == "Intern"){
+    //         fs.readFileSync("templates/intern.html", "utf8", (err, htmlFile) => {
+    //             if (err) { return console.log(err); }
+    //             //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
+    //         });
+    //     }
+    //     else if (team[i].getRole() == "Engineer"){
+    //         fs.readFileSync("templates/engineer.html", "utf8", (err, htmlFile) => {
+    //             if (err) { return console.log(err); }
+    //             //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
+    //         });
+    //     }
+    // }
+
+
+    //console.log(team[0].name);
+    //console.log(team[0].getRole());
+
+    console.log(teamHTML);
+    //generateTeamHTML(team);
+}
+
+async function generateTeamHTML(teamArray){
+    let teamHTML;
+
+    for (i=0; i < teamArray.length; i++){
+
+        if (teamArray[i].getRole() == "Manager"){
+            fs.readFile("templates/engineer.html", "utf8", function(err, htmlFile) {
+                if (err) { return console.log(err); }
+                console.log(teamArray[i].name);
+                console.log(teamArray[i].getRole);
+                console.log(teamArray[i].id);
+                console.log(teamArray[i].email);
+                console.log(teamArray[i].officeNumber);
+                //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
+            });
+        }
+        else if (teamArray[i].getRole() == "Intern"){
+            fs.readFile("templates/intern.html", "utf8", (err, htmlFile) => {
+                if (err) { return console.log(err); }
+                //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
+            });
+        }
+        else if (teamArray[i].getRole() == "Engineer"){
+            fs.readFile("templates/engineer.html", "utf8", (err, htmlFile) => {
+                if (err) { return console.log(err); }
+                //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
+            });
+        }
+    }
+
+    console.log(teamHTML);
 }
 
 start();
