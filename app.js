2752d0845a1eee75abd94d5b22a166091dc2e27a
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-06 17:20:38 -0800

    Removed unused code and added comments

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
+// ===== Node Packages =====
 const inquirer = require("inquirer");
 const fs = require("fs");
+
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 
-let team = [];
-let position = "";
-let teamSize;
-
-const managerQ = [
-    {
-        type: "input",
-        message: "What is your Manager's Office Number?",
-        name: "officeNo"
-    }
-];
-
-const internQ = [
-    {
-        type: "input",
-        message: "What school is your Intern attending?",
-        name: "school"
-    }
-];
-
-const engineerQ = [
-    {
-        type: "input",
-        message: "What is your Engineer's GitHub?",
-        name: "github"
-    }
-];
-
-
+// ===== START FUNCTION ===== 
 async function start(){
     console.log("Let's make your Dream Team!");
+
+    // Set Variable to hold HTML
     let teamHTML = "";
 
+    // Variable to hold number of team members
+    let teamSize;
+
+    // First Question to ask to set up loop
     await inquirer.prompt(
         {
             type: "number",
@@ -45,22 +25,28 @@ async function start(){
         }
     )
     .then((data) => {
+
+        // Number of team members placed in teamSize for scope purposes.
+        // 1 is added start from 1 rather than 0 for user understanding.
         teamSize = data.noOfTeamMem + 1;
     });
     
-    // If Team Size is 0
+    // If Team Size is 0, will end program
     if (teamSize === 0){
         console.log("I guess there is no one on your team...");
         return;
     }
     
+    // Loop begins to ask questions depending on the size of the team
     for(i = 1; i < teamSize; i++){
 
+        // Global variables set
         let name;
         let id;
         let title;
         let email;
 
+        // Prompts user to answer the basic questions of the employee
         await inquirer.prompt([ 
             {
                 type: "input",
@@ -85,52 +71,83 @@ async function start(){
             }
         ])
         .then((data) => {
+
+            // Takes data from user and places value in global variables
             name = data.name;
             id = data.id;
             title = data.title;
             email = data.email;
         });
 
+        // Switch Case depending on the title of the employee
         switch (title){
             case "Manager":
-                await inquirer.prompt(managerQ)
+
+                // ask user of Manager's Office Number
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Manager's Office Number?",
+                        name: "officeNo"
+                    }
+                ])
                 .then((data) => {
+
+                    // Create a new object with all avaiable user input data
                     const manager = new Manager(name, id, email, data.officeNo);
 
+                    // Reads and places HTML from manager.html in teamMemever Variable
                     teamMember = fs.readFileSync("templates/manager.html");
-            
-                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
 
+                    // Uses eval() to pass template literals from html files.
+                    // Adds the string to the team HTML.
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
                 });
                 break;
+
+            //Steps Similar to Manager but for intern
             case "Intern":
-                await inquirer.prompt(internQ)
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What school is your Intern attending?",
+                        name: "school"
+                    }
+                ])
                 .then((data) => {
                     const intern = new Intern(name, id, email, data.school);
-
                     teamMember = fs.readFileSync("templates/intern.html");
-            
                     teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
                 });
                 break;
+
+            //Steps Similar to Manager but for engineer
             case "Engineer":
-                await inquirer.prompt(engineerQ)
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Engineer's GitHub?",
+                        name: "github"
+                    }
+                ])
                 .then((data) => {
                     const engineer = new Engineer(name, id, email, data.github);
-
                     teamMember = fs.readFileSync("templates/engineer.html");
-            
                     teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
-
                 });
                 break;
-        }
-    }
 
+        } // End of Switch Case
+
+    } // End of For loop
+
+    // Reads main.html and places html in a variable
     const mainHTML = fs.readFileSync("templates/main.html");
-            
+    
+    // Use eval to implement template literals in main.html and places teamHTML inside main template
     teamHTML = eval('`'+ mainHTML +'`');
 
+    // write file to new team.html file
     fs.writeFile("output/team.html", teamHTML, function(err) {
 
         if (err) {
@@ -141,86 +158,8 @@ async function start(){
       
       });
 
-
-    //console.log(teamMember);
-
-    
-
-
-    // for (i=0; i < team.length; i++){
-    //     let teamMember;
-
-    //     if (team[i].getRole() === "Manager"){
-
-    //         // console.log(team[i].name);
-    //         // console.log(team[i].getRole());
-    //         // console.log(team[i].id);
-    //         // console.log(team[i].email);
-    //         // console.log(team[i].officeNumber);
-
-    //         teamMember = fs.readFileSync("templates/manager.html");
-            
-    //         teamHTML = teamHTML + eval('`'+ teamMember +'`');
-
-    //         // fs.readFileSync("templates/engineer.html", "utf8", (err, htmlFile) => {
-    //         //     if (err) { return console.log(err); }
-            
-    //         //     teamMember = eval('`'+ htmlFile +'`');
-    //         // });
-    //     }
-    //     else if (team[i].getRole() == "Intern"){
-    //         fs.readFileSync("templates/intern.html", "utf8", (err, htmlFile) => {
-    //             if (err) { return console.log(err); }
-    //             //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
-    //         });
-    //     }
-    //     else if (team[i].getRole() == "Engineer"){
-    //         fs.readFileSync("templates/engineer.html", "utf8", (err, htmlFile) => {
-    //             if (err) { return console.log(err); }
-    //             //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
-    //         });
-    //     }
-    // }
-
-
-    //console.log(team[0].name);
-    //console.log(team[0].getRole());
-
-    console.log(teamHTML);
-    //generateTeamHTML(team);
+    // console.log(teamHTML);
 }
 
-async function generateTeamHTML(teamArray){
-    let teamHTML;
-
-    for (i=0; i < teamArray.length; i++){
-
-        if (teamArray[i].getRole() == "Manager"){
-            fs.readFile("templates/engineer.html", "utf8", function(err, htmlFile) {
-                if (err) { return console.log(err); }
-                console.log(teamArray[i].name);
-                console.log(teamArray[i].getRole);
-                console.log(teamArray[i].id);
-                console.log(teamArray[i].email);
-                console.log(teamArray[i].officeNumber);
-                //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
-            });
-        }
-        else if (teamArray[i].getRole() == "Intern"){
-            fs.readFile("templates/intern.html", "utf8", (err, htmlFile) => {
-                if (err) { return console.log(err); }
-                //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
-            });
-        }
-        else if (teamArray[i].getRole() == "Engineer"){
-            fs.readFile("templates/engineer.html", "utf8", (err, htmlFile) => {
-                if (err) { return console.log(err); }
-                //teamHTML = teamHTML + eval('`'+ htmlFile +'`');
-            });
-        }
-    }
-
-    console.log(teamHTML);
-}
 
 start();
